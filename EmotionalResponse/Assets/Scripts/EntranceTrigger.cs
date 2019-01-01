51886caa6866329da35bf30d794b8a29aab04578
Alex Stroud <alexander2.stroud@live.uwe.ac.uk>
2019-01-01 18:37:26 +0000

    Added entrance tree with trigger to display

---
+++ EmotionalResponse/Assets/Scripts/EntranceTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceTrigger : MonoBehaviour {
+
+    private GameObject tree;
+    void Start()
+    {
+        tree = GameObject.FindGameObjectWithTag("EntranceDoor").gameObject;
+        tree.SetActive(false);
+    }
+
+void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //add audio cues here for tree growing
+            tree.SetActive(true);
+        }
+    }
+}
