0f93e6475c4548d8fca105b15f053842439b0945
Alex Stroud <alexander2.stroud@live.uwe.ac.uk>
2019-02-26 21:36:39 +0000

    Added pile of boxes to fall over behind player

---
+++ EmotionalResponse/Assets/CrateCollapse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateCollapse : MonoBehaviour {
+
+    public GameObject OldBoxPile;
+    public GameObject newBoxPile;
+    public AudioSource audiosource;
+
+	// Use this for initialization
+	void Start () {
+        newBoxPile.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            audiosource.time = 0.0f;
+            audiosource.Play();
+            OldBoxPile.SetActive(false);
+            newBoxPile.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+}
