bac6013180762de6d033dd0ac07985b05889930f
Alex Stroud <alexander2.stroud@live.uwe.ac.uk>
2019-03-07 15:51:43 +0000

    Added new torch sprite + added offset for moving torch around

---
+++ EmotionalResponse/Assets/OffsetTorch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetTorch : MonoBehaviour {
+
+    private Vector3 offset;
+    private GameObject followTarget;
+    [SerializeField] private float speed = 3.0f;
+
+	void Start () {
+        followTarget = Camera.main.gameObject;
+        offset = transform.position - followTarget.transform.position;  //allow torch to follow camera with a sway motion
+	}
+	
+	void Update () {
+        transform.position = followTarget.transform.position + offset;
+        transform.rotation = Quaternion.Slerp(transform.rotation, followTarget.transform.rotation, speed * Time.deltaTime);     //smoothly rotate torch
+	}
+}
