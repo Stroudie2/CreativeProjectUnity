d51dead17ba40860960ba3e7f95d8782038796d4
Alex Stroud <alexander2.stroud@live.uwe.ac.uk>
2019-03-27 00:24:42 +0000

    Uncommented audio code

--- EmotionalResponse/Assets/AudioManager.cs
+++ EmotionalResponse/Assets/AudioManager.cs
@@ -15,33 +15,33 @@ public class AudioManager : MonoBehaviour {
 
     public void PlayPipeSound()
     {
-        //if (controller.nearbyPipes.Count > 0)   //checks pipes around player
-        //{
-        //    GameObject selectedPipe = controller.nearbyPipes[Random.Range(0, controller.nearbyPipes.Count)];    //pick random pipe from around player
-        //    if ((selectedPipe == lastPlayedPipe) & (controller.nearbyPipes.Count > 1))  //don't play sound at same location twice
-        //    {
-        //        while (selectedPipe == lastPlayedPipe)
-        //        {
-        //            selectedPipe = controller.nearbyPipes[Random.Range(0, controller.nearbyPipes.Count - 1)];   //forces to pick another pipe
-        //        }
+        if (controller.nearbyPipes.Count > 0)   //checks pipes around player
+        {
+            GameObject selectedPipe = controller.nearbyPipes[Random.Range(0, controller.nearbyPipes.Count)];    //pick random pipe from around player
+            if ((selectedPipe == lastPlayedPipe) & (controller.nearbyPipes.Count > 1))  //don't play sound at same location twice
+            {
+                while (selectedPipe == lastPlayedPipe)
+                {
+                    selectedPipe = controller.nearbyPipes[Random.Range(0, controller.nearbyPipes.Count - 1)];   //forces to pick another pipe
+                }
 
-        //    }
-        //    else if (selectedPipe != lastPlayedPipe)    //use this pipe
-        //    {
-        //        lastPlayedPipe = selectedPipe;  //update this as the last played pipe too
-        //        while (selectedPipe.GetComponent<AudioSource>().clip == lastPlayedClip)     //regenerate whilst it's the same clip
-        //        {
-        //            selectedPipe.GetComponent<AudioSource>().clip = pipeAudioClips[Random.Range(0, pipeAudioClips.Count - 1)];  //choose random pipe sound
-        //        }
-        //        lastPlayedClip = selectedPipe.GetComponent<AudioSource>().clip;
-        //        selectedPipe.GetComponent<AudioSource>().Play();
-        //        selectedPipe.GetComponentInChildren<ParticleSystem>().Emit(1);  //emit smoke once
-        //    }
-        //}
+            }
+            else if (selectedPipe != lastPlayedPipe)    //use this pipe
+            {
+                lastPlayedPipe = selectedPipe;  //update this as the last played pipe too
+                while (selectedPipe.GetComponent<AudioSource>().clip == lastPlayedClip)     //regenerate whilst it's the same clip
+                {
+                    selectedPipe.GetComponent<AudioSource>().clip = pipeAudioClips[Random.Range(0, pipeAudioClips.Count - 1)];  //choose random pipe sound
+                }
+                lastPlayedClip = selectedPipe.GetComponent<AudioSource>().clip;
+                selectedPipe.GetComponent<AudioSource>().Play();
+                selectedPipe.GetComponentInChildren<ParticleSystem>().Emit(1);  //emit smoke once
+            }
+        }
 
 
-        //playWaitTime = Random.Range(3f, 15f);   //wait a random time period before next sound can be played
-        //lastPlayedTime = Time.time;
+        playWaitTime = Random.Range(3f, 15f);   //wait a random time period before next sound can be played
+        lastPlayedTime = Time.time;
     }
 
     void Update()
